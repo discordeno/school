82b3210e2083fd99747e21a1ffb3945820cd9bac
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-05-30 18:41:40 +0000

    fix: manager

--- src/client/actions/ActionsManager.ts
+++ src/client/actions/ActionsManager.ts
@@ -1,43 +1,84 @@
+import MessageCreate from "./MessageCreate.ts";
+import MessageDelete from "./MessageDelete.ts";
+import MessageDeleteBulk from "./MessageDeleteBulk.ts";
+import MessageUpdate from "./MessageUpdate.ts";
+import MessageReactionAdd from "./MessageReactionAdd.ts";
+import MessageReactionRemove from "./MessageReactionRemove.ts";
+import MessageReactionRemoveAll from "./MessageReactionRemoveAll.ts";
+import MessageReactionRemoveEmoji from "./MessageReactionRemoveEmoji.ts";
+import ChannelCreate from "./ChannelCreate.ts";
+import ChannelDelete from "./ChannelDelete.ts";
+import ChannelUpdate from "./ChannelUpdate.ts";
+import GuildDelete from "./GuildDelete.ts";
+import GuildUpdate from "./GuildUpdate.ts";
+import InviteCreate from "./InviteCreate.ts";
+import InviteDelete from "./InviteDelete.ts";
+import GuildMemberRemove from "./GuildMemberRemove.ts";
+import GuildMemberUpdate from "./GuildMemberUpdate.ts";
+import GuildBanRemove from "./GuildBanRemove.ts";
+import GuildRoleCreate from "./GuildRoleCreate.ts";
+import GuildRoleDelete from "./GuildRoleDelete.ts";
+import GuildRoleUpdate from "./GuildRoleUpdate.ts";
+import PresenceUpdate from "./PresenceUpdate.ts";
+import UserUpdate from "./UserUpdate.ts";
+import VoiceStateUpdate from "./VoiceStateUpdate.ts";
+import GuildEmojiCreate from "./GuildEmojiCreate.ts";
+import GuildEmojiDelete from "./GuildEmojiDelete.ts";
+import GuildEmojiUpdate from "./GuildEmojiUpdate.ts";
+import GuildEmojisUpdate from "./GuildEmojisUpdate.ts";
+import GuildRolesPositionUpdate from "./GuildRolesPositionUpdate.ts";
+import GuildChannelsPositionUpdate from "./GuildChannelsPositionUpdate.ts";
+import GuildIntegrationsUpdate from "./GuildIntegrationsUpdate.ts";
+import WebhooksUpdate from "./WebhooksUpdate.ts";
+import TypingStart from "./TypingStart.ts";
+import Client from "../Client.ts";
+
 export class ActionsManager {
-  constructor(client) {
+  client: Client;
+
+  // deno-lint-ignore no-explicit-any
+  [key: string]: any;
+
+  constructor(client: Client) {
     this.client = client;
 
-    this.register(require("./MessageCreate"));
-    this.register(require("./MessageDelete"));
-    this.register(require("./MessageDeleteBulk"));
-    this.register(require("./MessageUpdate"));
-    this.register(require("./MessageReactionAdd"));
-    this.register(require("./MessageReactionRemove"));
-    this.register(require("./MessageReactionRemoveAll"));
-    this.register(require("./MessageReactionRemoveEmoji"));
-    this.register(require("./ChannelCreate"));
-    this.register(require("./ChannelDelete"));
-    this.register(require("./ChannelUpdate"));
-    this.register(require("./GuildDelete"));
-    this.register(require("./GuildUpdate"));
-    this.register(require("./InviteCreate"));
-    this.register(require("./InviteDelete"));
-    this.register(require("./GuildMemberRemove"));
-    this.register(require("./GuildMemberUpdate"));
-    this.register(require("./GuildBanRemove"));
-    this.register(require("./GuildRoleCreate"));
-    this.register(require("./GuildRoleDelete"));
-    this.register(require("./GuildRoleUpdate"));
-    this.register(require("./PresenceUpdate"));
-    this.register(require("./UserUpdate"));
-    this.register(require("./VoiceStateUpdate"));
-    this.register(require("./GuildEmojiCreate"));
-    this.register(require("./GuildEmojiDelete"));
-    this.register(require("./GuildEmojiUpdate"));
-    this.register(require("./GuildEmojisUpdate"));
-    this.register(require("./GuildRolesPositionUpdate"));
-    this.register(require("./GuildChannelsPositionUpdate"));
-    this.register(require("./GuildIntegrationsUpdate"));
-    this.register(require("./WebhooksUpdate"));
-    this.register(require("./TypingStart"));
+    this.register(MessageCreate);
+    this.register(MessageDelete);
+    this.register(MessageDeleteBulk);
+    this.register(MessageUpdate);
+    this.register(MessageReactionAdd);
+    this.register(MessageReactionRemove);
+    this.register(MessageReactionRemoveAll);
+    this.register(MessageReactionRemoveEmoji);
+    this.register(ChannelCreate);
+    this.register(ChannelDelete);
+    this.register(ChannelUpdate);
+    this.register(GuildDelete);
+    this.register(GuildUpdate);
+    this.register(InviteCreate);
+    this.register(InviteDelete);
+    this.register(GuildMemberRemove);
+    this.register(GuildMemberUpdate);
+    this.register(GuildBanRemove);
+    this.register(GuildRoleCreate);
+    this.register(GuildRoleDelete);
+    this.register(GuildRoleUpdate);
+    this.register(PresenceUpdate);
+    this.register(UserUpdate);
+    this.register(VoiceStateUpdate);
+    this.register(GuildEmojiCreate);
+    this.register(GuildEmojiDelete);
+    this.register(GuildEmojiUpdate);
+    this.register(GuildEmojisUpdate);
+    this.register(GuildRolesPositionUpdate);
+    this.register(GuildChannelsPositionUpdate);
+    this.register(GuildIntegrationsUpdate);
+    this.register(WebhooksUpdate);
+    this.register(TypingStart);
   }
 
-  register(Action) {
+  // deno-lint-ignore no-explicit-any
+  register(Action: any) {
     this[Action.name.replace(/Action$/, "")] = new Action(this.client);
   }
 }
